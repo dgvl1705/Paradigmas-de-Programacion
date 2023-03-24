517133d5ac5035f3349e2a32ddf810f5998aae99
Diego Gamaliel Vargas Lopez <diego.vargas.edu@gmail.com>
2023-03-24 12:31:28 -0600

    Termino de segunda parte de intro.py

--- intro.py
+++ intro.py
@@ -35,7 +35,9 @@ print("vamos a sumar esto" + " con esto otro")
 #===============================================
 # Continuar una instrucción en varios renglones
 #===============================================
-if 100 > 99 and 200 <= 300 and True != False:
+if 100 > 99 and \
+    200 <= 300 and \
+    True != False:
         print('Hello World!')
 
 #================================================
@@ -87,4 +89,80 @@ say_hello("Diego")
 # Input permite obtener datos del usuario en prompter
 #=======================================================
 nombre=input("Dame tu nombre: ")
-print(
+print("Hola como estas", nombre)
+
+#=======================================================
+# Los enteros son de precisión ilimitada
+#=======================================================
+
+y = 5000000000000000000000000000000
+print(y)
+
+#============================================================
+# Se puede delimitar números con guión bajo pero no con coma
+#============================================================
+y = 5_000_000
+print(y)
+
+#============================================================
+# La función int() cambia strings y floats a enteros
+#============================================================
+numero =  int(input("Dame tu edad: "))
+type(numero)
+
+#============================================================
+# La notación científica de flotantes utiliza e o E
+#============================================================
+# 1.2 x 10^{-9}
+#=======================
+y = 1.2E-09
+print(y)
+
+#============================================================
+# La función float() convierte strings y enteros a floats
+#============================================================
+y = float("14.3")
+print(y)
+
+#===========================================================
+# Los complejos  se escriben con la raíz de menos uno
+# j siempre  con un número como prefijo
+# no acepta la j suelta
+#===========================================================
+z = 1+1j
+
+# suma +
+# resta -
+# multiplicación *
+# división /
+# módulo %
+# esponente **
+# función piso //
+# Funciones  para transformar números int() float() complex()
+# Operaciones abs() round() pow()
+
+print(round(3.14159, 4))
+
+#=======================================
+# String de varias líneas
+#=======================================
+
+parrafo = """ En el bosque de la china
+ la chinita se perdió """
+print(parrafo)
+
+#================================================
+# La función len() obtiene el tamaño del string
+#================================================
+
+n=len(parrafo)
+print(n)
+
+#===================================================================
+# Las letras de un string ocupan lugares como en un arreglo
+# (también de atrás para adelante comenzando en -1 el último)
+#===================================================================
+
+palabra = "hola"
+print(palabra[0])
+print(palabra[-4])
