64ea8e69370e875289084877e253cbff44274821
Diego Gamaliel Vargas Lopez <diego.vargas.edu@gmail.com>
2023-03-27 11:51:16 -0600

    Incio de laboratorio parte 2

--- intro.py
+++ intro.py
@@ -166,3 +166,53 @@ print(n)
 palabra = "hola"
 print(palabra[0])
 print(palabra[-4])
+
+
+#================================================
+# Conjunto en  python
+#================================================
+even_nums = {2, 4, 6, 8, 10} #Conjunto  de números pares
+print(even_nums)
+
+# El bool no es parte del conjunto
+emp = {1, 'Steve', 10.5, True} # Conjunto de diferentes objetos
+print(emp)
+
+nums = {1, 2, 2, 3, 4, 4, 5, 5}
+print(nums)
+
+#======================================================
+# Convertir secuencia a conjunto 
+# No lo genera en orden
+#======================================================
+s = set('Hello')
+print(s)
+s = set((1, 2, 3, 4, 5)) # Tupla a conjunto
+print(s)
+
+#=====================================================
+# De diccionario a conjunto: conjunto de llaves
+#=====================================================
+d = {1: 'One', 2: 'Two'}
+s = set(d)
+print(s)
+
+s.add(100)
+print(s)
+
+s.update(nums)
+print(s)
+
+s.remove(100)
+print(s)
+
+s1 = {1, 2, 3, 4, 5}
+s2 = {4, 5, 6, 7, 8}
+
+su = s1|s2 # Unión
+print(su)
+
+si = s1&s2 # Intersección
+print(si)
+
+
