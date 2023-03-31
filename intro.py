7954c8f4b8deeb35e5024ad3f75a443b6a6ee3b6
dgvl1705 <diego.vargas.edu@gmail.com>
2023-03-31 12:31:00 -0600

    Termino del primer archivo del laboratorio 2 e inicio del segundo archivo

--- intro.py
+++ intro.py
@@ -215,4 +215,83 @@ print(su)
 si = s1&s2 # Intersección
 print(si)
 
+sr =  s1 - s2 #Diferencia de conjuntos
+print(sr)
 
+sp = s2 - s1
+print(sp)
+
+ss = s1^s2 #Diferencia simétrica
+print(ss)
+
+
+#==========================================================
+# Uso de diccionarios
+#==========================================================
+capitals = {"USA":"Washington D.C.", "France": "Paris", "India":"New Delhi"}
+print(capitals)
+
+
+#===========================================================
+# llave: valor
+#===========================================================
+# diccionario vacío
+d = {}
+
+# Llave entera, valor string
+numNames = {1: "One", 2: "Two", 3: "Three"}
+
+# Llave real, valor string
+decNames = {1.5: "One and Half", 2.5: "Two and Half", 3.5: "Three and Half"}
+
+# Llave tupla, valor string
+items = {("Parker", "Reynolds", "Camlin" ): "pen", ("LG", "Whirlpool", "Samsung"): "Refrigerator"}
+
+# Llave string, valor int
+romanNums = {'I': 1, 'II': 2, 'III': 3, 'IV': 4, 'V': 5}
+print(romanNums)
+print(romanNums["I"])
+
+print(capitals.get("India"))
+print(capitals.get("india"))
+
+# Reportar llave y valor
+for k in capitals:
+    print("Key = " + k + ", Value = " + capitals[k])
+
+#Nuevo dato para el diccionario
+capitals["Mexico"] = "CDMX"
+print(capitals)
+
+# Borrar dato del diccionario
+del capitals["Mexico"]
+print(capitals)
+
+# Borrar todo el diccionario
+del capitals
+ 
+# Reportar llaves
+print(romanNums.keys())
+
+# Reportar valores
+print(romanNums.values())
+
+# Juicio de llave (está o no está la llave en el diccionario)
+print("I" in romanNums)
+print("X" in romanNums)
+print("XX" in romanNums)
+
+
+
+#=========================================================
+# Listas
+# Las listas pueden ser de objetos diferentes
+#=========================================================
+miprimeralista = [] # Lsita vacía
+print(miprimeralista)
+
+#========================================================
+# Llenado de lista
+#========================================================
+miprimeralista = [1, "Javier", 1.34, "Bosco", "Angel", "Abigail", True]
+print(miprimeralista)
