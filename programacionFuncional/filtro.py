ca80d5a46165348ab1b178f31cc48c9b2b6feaab
dgvl1705 <diego.vargas.edu@gmail.com>
2023-05-22 12:16:49 -0600

    Termino del laboratorio 7

---
+++ programacionFuncional/filtro.py
@@ -0,0 +1,30 @@
+#======================
+# Uso de filtros
+#======================
+
+#========================================================
+# Hacer una lista de los números por arriba del promedio
+#========================================================
+
+# Módulo de estadística
+import statistics
+
+bigdata = [1.3, 2.7, 0.8, 4.1, 4.3, -0.1]
+promedio = statistics.mean(bigdata)
+print(promedio)
+
+#=====================================================================
+# Hazme una lista de elementos que cumplan la condición x > promedio
+# filter( condición, datos )
+#=====================================================================
+print(list(filter(lambda x: x > promedio, bigdata)))
+
+#====================
+# Limpiar los datos
+#====================
+paises = ["", "Argentina", "", "Brasil", "", "Chile", "México", "", "Colombia", "", "", "Cuba", "Venezuela"]
+
+#==================================
+# Filtrar lo que no contiene nada
+#==================================
+print(list(filter(None, paises)))
